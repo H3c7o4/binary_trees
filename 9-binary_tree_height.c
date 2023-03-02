fdad82c168d212366a0f7d8c8e37a8e539aad477
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-02 06:47:07 +0100

    added 9-binary_tree_height.c file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+size_t check(const binary_tree_t *node, size_t height);
 /**
  * binary_tree_height - A function that measures the height
  * of a binary tree
@@ -9,41 +10,38 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
-	left_height = 0;
-	right_height = 0;
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		left_height = count(tree->left, 0);
-	if (tree->right != NULL)
-		right_height = count(tree->right, 0);
-	if (left_height > right_height)
-		return (left_height);
-	return (right_height);
+	return (check(tree, 0));
 }
 
 
 /**
- * count - A function that counts the nodes
+ * check - A function that counts the nodes
  * of a binary tree
  * @node: the pointer to the parent node
  * @height: the pointer to the height of the tree
- * Return: size_t
+ * Return: the height of the binary tree
  */
-size_t count(const binary_tree_t *node, size_t height)
+size_t check(const binary_tree_t *node, size_t height)
 {
-	size_t new_height;
+	size_t new_height, left_height, right_height;
 
-	new_height = height + 1;
+	new_height = 0;
+	left_height = 0;
+	right_height = 0;
 	if (node->left == NULL && node->right == NULL)
-		return (new_height);
+	{
+		return (height);
+	}
+	new_height = height + 1;
+	/* check left */
 	if (node->left != NULL)
-		if ((node->left)->left == NULL && (node->left)->right == NULL)
-			count(node->left, new_height);
+		left_height = check(node->left, new_height);
+	/* check right */
 	if (node->right != NULL)
-		if ((node->right)->left == NULL && (node->right)->right == NULL)
-			count(node->right, new_height);
-	return (new_height + 1);
+		right_height = check(node->right, new_height);
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
 }
