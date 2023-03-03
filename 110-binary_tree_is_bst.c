3c52aac6ea2d5b5397ae8c41f3c76aab43729dfc
callmhejerry <chinedujeremiah2002@gmail.com>
2023-03-03 23:43:23 +0100

    hot fix

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,8 +1,6 @@
 #include "binary_trees.h"
 
 
-int check_side(const binary_tree_t *tree, char side, int root);
-
 /**
  * binary_tree_is_bst - A function that checks if a binary tree is
  * a valid Binary search tree
@@ -13,99 +11,80 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (is_bst(tree, tree->n) == 0)
-		return (0);
-	if (tree->left != NULL)
-		if (binary_tree_is_bst(tree->left) == 0)
-			return (0);
-	if (tree->right != NULL)
-		if (binary_tree_is_bst(tree->right) == 0)
-			return (0);
-	return (1);
+	return (is_bst(tree));
 }
 
 /**
  * is_bst - A function that checks if a binary tree is
  * a valid Binary search tree
  * @tree: the pointer to the root node of the tree to check
- * @root: the data stored at the root node
  * Return: 1 if tree is a valid BST and 0 otherwise
- * if tree is NULL return 0
+ * if tree is NULL return 1
  */
-int is_bst(const binary_tree_t *tree, int root)
+int is_bst(const binary_tree_t *tree)
 {
-	int ch;
-
-	ch = 0;
-
 	if (tree == NULL)
 		return (1);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	if (tree->parent == NULL)
-	{
-		if (tree->left != NULL)
-			if ((tree->left)->n > tree->n)
-				return (0);
-		if (tree->right != NULL)
-			if ((tree->right)->n < tree->n)
-				return (0);
-	}
-	else
-	{
-		ch = check_side(tree, 'R', root);
-		if (ch == 0)
-			return (0);
-		ch = check_side(tree, 'L', root);
-		if (ch == 0)
-			return (0);
-	}
-	if (tree->left != NULL && tree->right != NULL)
-		if ((tree->left)->n == (tree->right)->n)
-			return (0);
-	ch = is_bst(tree->left, root);
-	if (ch == 0)
+
+	if (tree->left != NULL && (is_max(tree->left) > tree->n))
 		return (0);
-	ch = is_bst(tree->right, root);
-	if (ch == 0)
+	if (tree->right != NULL && (is_min(tree->right) < tree->n))
+		return (0);
+	if (!is_bst(tree->left) || !is_bst(tree->right))
 		return (0);
 	return (1);
 }
 
+
 /**
- * check_side - A function that checks the left side of the
- * binary tree to see if a binary tree is a BST
+ * is_max - A function that returns the biggest number
+ * in a binary tree
  * @tree: the pointer to the root of the tree
- * @side: "L" or "R" to show which side to check R is right
- * and L is left
- * @root: the data stored in the root of the tree
- * Return: 0 if the side is not a BST
+ * Return: the biggest number
  */
-int check_side(const binary_tree_t *tree, char side, int root)
+int is_max(const binary_tree_t *tree)
 {
-	if (side == 'R')
-	{
-		if (tree->n > root) /*right side*/
-		{
-			if (tree->left != NULL && ((tree->left)->n > tree->n
-						|| (tree->left)->n < root))
-				return (0);
-			if (tree->right != NULL && ((tree->right)->n < tree->n
-						|| (tree->right)->n < root))
-				return (0);
-		}
-	}
-	else if (side == 'L')
-	{
-		if (tree->n < root) /*left side*/
-		{
-			if (tree->left != NULL && ((tree->left)->n > tree->n
-						|| (tree->left)->n > root))
-				return (0);
-			if (tree->right != NULL && ((tree->right)->n < tree->n
-						|| (tree->right)->n > root))
-				return (0);
-		}
-	}
-	return (1);
+	int left, right, biggest;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (tree->n);
+
+	left = is_max(tree->left);
+	right = is_max(tree->right);
+
+	if (left > right)
+		biggest = left;
+	else
+		biggest = right;
+	if (biggest > tree->n)
+		return (biggest);
+	return (tree->n);
+}
+
+
+/**
+ * is_min - A function that returns the smallest number in
+ * a binary tree
+ * @tree: the pointer to the root of the tree
+ * Return: the smallest number in the tree
+ */
+int is_min(const binary_tree_t *tree)
+{
+	int left, right, smallest;
+
+	if (tree == NULL)
+		return (1000000);
+	if (tree->left == NULL && tree->right == NULL)
+		return (tree->n);
+	left = is_min(tree->left);
+	right = is_min(tree->right);
+	if (left < right)
+		smallest = left;
+	else
+		smallest = right;
+	if (smallest < tree->n)
+		return (smallest);
+	return (tree->n);
 }
